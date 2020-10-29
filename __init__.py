d113272e5837ebdfe499c45532a54b539e0c8584
Jannik Smidt <js343@hdm-stuttgart.de>
2020-10-29 14:26:56 +0100

    testing ros2 connection with talker

--- __init__.py
+++ __init__.py
@@ -1,13 +1,38 @@
 from mycroft import MycroftSkill, intent_file_handler
+import rclpy
+from rclpy.node import Node
 
+from std_msgs.msg import String
 
 class MarcoChessVoice(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        main()
 
     @intent_file_handler('voice.chess.marco.intent')
     def handle_voice_chess_marco(self, message):
         self.speak_dialog('voice.chess.marco')
+
+class Listener(Node):
+
+    def __init__(self):
+        super().__init__('listener')
+        self.sub = self.create_subscription(String, 'chatter', self.chatter_callback, 10)
+
+    def chatter_callback(self, msg):
+        self.get_logger().info('I heard: [%s]' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = Listener()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 def create_skill():
