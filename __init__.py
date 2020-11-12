008260af44f56f656611a5e26e18b010800be002
Jannik Smidt <js343@hdm-stuttgart.de>
2020-11-12 15:03:50 +0100

    background collection

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+import asyncio
+import threading
+
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 from adapt.intent import IntentBuilder
 import rclpy
@@ -6,17 +9,19 @@ from rclpy.node import Node
 from string_interface.srv import MARCO
 from std_msgs.msg import String
 
+
 class MarcoChessVoice(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         rclpy.init(args=None)
         self.log.info('*********** init ************')
         self.client = self.MarcoClientAsync()
+        self.last_emotion = ''
+        self.last_identity = ''
+        self.AsyncIdentityUpdater(self)
+        self.AsyncEmotionUpdater(self)
 
-    @intent_file_handler('voice.chess.marco.intent')
-    def handle_voice_chess_marco(self, message):
-        self.client.send_request('Hello')
-
+    def ros2_call(self) -> String:
         while rclpy.ok():
             rclpy.spin_once(self.client)
             if self.client.future.done():
@@ -27,23 +32,28 @@ class MarcoChessVoice(MycroftSkill):
                 else:
                     self.log.info('Result: {}'.format(response.res))
                 break
-        self.speak('Hello {}'.format(response))
+        return response.res
+
+    @intent_handler(IntentBuilder('Emotion')
+                    .require('what')
+                    .require('is')
+                    .require('my')
+                    .require('emotion'))
+    def handle_voice_chess_marco(self, message):
+        self.client.request_emotion('Hello')
+
+        response = self.ros2_call()
+
+        self.speak('Current Emotion is: {}'.format(response))
 
     @intent_handler(IntentBuilder('Greeting')
                     .require('hello'))
     def handle_voice_chess_marco(self, message):
 
         self.client.request_identity('Current')
-        while rclpy.ok():
-            rclpy.spin_once(self.client)
-            if self.client.future.done():
-                try:
-                    response = self.client.future.result()
-                except Exception as e:
-                    self.log.info('Service call failed {}'.format(e,))
-                else:
-                    self.log.info('Result: {}'.format(response.res))
-                break
+
+        response = self.ros2_call()
+
         self.speak('Hello {}'.format(response))
 
     class MarcoClientAsync(Node):
@@ -67,6 +77,38 @@ class MarcoChessVoice(MycroftSkill):
         def request_identity(self, request):
             self.req.req = request
             self.future = self.identity_client.call_async(self.req)
+
+    class AsyncIdentityUpdater(object):
+        def __init__(self, outer_instance):
+            self.outer_instance = outer_instance
+            thread = threading.Thread(target=self.run, args=())
+            thread.daemon = True
+            thread.start()
+
+        def run(self):
+            asyncio.run(self.update_identity())
+
+        async def update_identity(self):
+            while True:
+                self.outer_instance.client.request_identity('Current')
+                self.outer_instance.last_emotion = self.outer_instance.ros2_call()
+                await asyncio.sleep(5)
+
+    class AsyncEmotionUpdater(object):
+        def __init__(self, outer_instance):
+            self.outer_instance = outer_instance
+            thread = threading.Thread(target=self.run, args=())
+            thread.daemon = True
+            thread.start()
+
+        def run(self):
+            asyncio.run(self.update_emotion())
+
+        async def update_emotion(self):
+            while True:
+                self.outer_instance.client.request_emotion('Current')
+                self.outer_instance.last_emotion = self.outer_instance.ros2_call()
+                await asyncio.sleep(1)
 
 
 def create_skill():
