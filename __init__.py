7adb8d4968975cd3ca889ff6d454e77c13b7a0a0
Jannik Smidt <js343@hdm-stuttgart.de>
2020-11-25 12:47:13 +0100

    german dialog, fixes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import asyncio
 import threading
 import sys
+import time
 
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 from adapt.intent import IntentBuilder
@@ -32,6 +33,7 @@ class MarcoChessVoice(MycroftSkill):
         # init the stockfish chess engine
         self.active_game = False
         self.player_started = False
+        self.converse = True
         self.last_evaluation = 0.0
         self.stockfish = Stockfish(
             "/home/human/Desktop/stockfish12",
@@ -60,28 +62,35 @@ class MarcoChessVoice(MycroftSkill):
 
     @intent_handler('voice.chess.marco.startGame.intent')
     def start_chess_game(self, message):
-        self.active_game = True
-        player_start = self.ask_yesno('voice.chess.marco.askStart',
-                                      data={'name': self.last_identity})
-        if player_start == 'no':
-            self.speak_dialog('voice.chess.marco.start',
-                              data={'name': 'I'},
-                              wait=True)
-            first_move = self.stockfish.get_best_move()
-            self.move_list.append(first_move)
-            self.speak_dialog('voice.chess.marco.moveFigure',
-                              data={'start': first_move[:2],
-                                    'end': first_move[2:]},
-                              wait=True)
-            self.stockfish.set_position(self.move_list)
+        self.converse = False
+        if not self.active_game:
+            self.active_game = True
+            player_start = self.ask_yesno('voice.chess.marco.askStart',
+                                          data={'name': self.last_identity})
+            if player_start == 'no':
+                self.speak_dialog('voice.chess.marco.start',
+                                  data={'name': 'I'},
+                                  wait=True)
+                first_move = self.stockfish.get_best_move()
+                self.move_list.append(first_move)
+                self.speak_dialog('voice.chess.marco.moveFigure',
+                                  data={'start': first_move[:2],
+                                        'end': first_move[2:]},
+                                  wait=True)
+                self.stockfish.set_position(self.move_list)
+            else:
+                self.player_started = True
+                self.speak_dialog('voice.chess.marco.start',
+                                  data={'name': self.last_identity},
+                                  wait=True)
         else:
-            self.player_started = True
-            self.speak_dialog('voice.chess.marco.start',
-                              data={'name': 'you'},
+            self.speak_dialog('voice.chess.marco.alreadyPlaying',
                               wait=True)
+        self.converse = True
 
     @intent_handler('voice.chess.marco.moveFigure.intent')
     def move_figure(self, message):
+        self.converse = False
         if self.active_game:
             start = message.data.get('start')
             end = message.data.get('end')
@@ -97,33 +106,46 @@ class MarcoChessVoice(MycroftSkill):
                 current_evaluation = float(
                     self.stockfish.get_stockfish_evaluation())
 
-                g_move = f'voice.chess.marco.emotion.{self.last_emotion}.good'
+                good_move = f'voice.chess.marco.emotion.{self.last_emotion}.goodMove'
+                bad_move = f'voice.chess.marco.emotion.{self.last_emotion}.badMove'
                 if self.player_started:
                     if self.last_evaluation > current_evaluation:
-                        self.speak_dialog(g_move, wait=True)
+                        self.speak_dialog(good_move, wait=True)
                     else:
-                        self.speak_dialog('voice.chess.marco.badMove',
+                        self.speak_dialog(bad_move,
                                           wait=True)
                 else:
                     if self.last_evaluation < current_evaluation:
-                        self.speak_dialog(g_move, wait=True)
+                        self.speak_dialog(good_move, wait=True)
                     else:
-                        self.speak_dialog('voice.chess.marco.badMove',
+                        self.speak_dialog(bad_move,
                                           wait=True)
 
                 marco_move = self.stockfish.get_best_move_time(10000)
-                self.move_list.append(marco_move)
-                self.speak_dialog('voice.chess.marco.moveFigure',
-                                  data={'start': marco_move[:2],
-                                        'end': marco_move[2:]})
-                self.stockfish.set_position(self.move_list)
+                if marco_move:
+                    self.move_list.append(marco_move)
+                    self.speak_dialog('voice.chess.marco.moveFigure',
+                                      data={'start': marco_move[:2],
+                                            'end': marco_move[2:]})
+                    self.stockfish.set_position(self.move_list)
+                    # sleep for 2 second after the move to wait for emotions
+                    time.sleep(2)
+                    own_move = f'voice.chess.marco.emotion.{self.last_emotion}.ownTurn'
+                    self.speak_dialog(own_move,
+                                      data={'name': self.last_identity},
+                                      wait=True)
+                else:
+                    self.speak_dialog('voice.chess.marco.lost',
+                                      wait=True)
+                    self.active_game = False
 
             else:
-                self.speak('This is an invalid move',
-                           wait=True)
+                self.speak_dialog('voice.chess.marco.invalidMove',
+                                  wait=True)
         else:
-            self.speak('We are currently not playing chess',
-                       wait=True)
+            self.speak_dialog('voice.chess.marco.notPlaying',
+                              wait=True)
+        self.converse = True
 
     @intent_handler(IntentBuilder('getBoard')
                     .require('show')
@@ -132,8 +154,17 @@ class MarcoChessVoice(MycroftSkill):
         if self.active_game:
             self.log.info(self.stockfish.get_board_visual())
         else:
-            self.speak('We are currently not playing chess',
-                       wait=True)
+            self.speak_dialog('voice.chess.marco.notPlaying',
+                              wait=True)
+
+    @intent_handler('voice.chess.marco.surrender.intent')
+    def surrender(self, message):
+        if self.active_game:
+            self.active_game = False
+            self.speak_dialog('voice.chess.marco.surrender',
+                              wait=True)
+        self.speak_dialog('voice.chess.marco.notPlaying',
+                          wait=True)
 
     def ros2_call(self) -> String:
         try:
@@ -143,13 +174,13 @@ class MarcoChessVoice(MycroftSkill):
                     try:
                         response = self.client.future.result()
                     except Exception as e:
-                        self.log.info('Service call failed {}'.format(e, ))
+                        self.log.warning('Service call failed {}'.format(e, ))
                         return ''
                     else:
                         self.log.info('Result: {}'.format(response.res))
                         return response.res
         except:
-            self.log.info('rclpy not ready')
+            self.log.warning('rclpy not ready')
             return ''
 
     class MarcoClientAsync(Node):
@@ -195,10 +226,10 @@ class MarcoChessVoice(MycroftSkill):
                 if current_identity == 'NoFace' and self.no_face:
                     self.outer_instance.speak('I can not see you',
                                               wait=True)
-                    
+
                 # if no face is detected and there is a previous face detected,
                 # use that face again remember that no face was detected
-                elif current_identity == 'noFace' and not self.no_face:
+                elif current_identity == 'NoFace' and not self.no_face:
                     self.no_face = True
                 else:
                     self.outer_instance.last_identity = current_identity
@@ -218,7 +249,9 @@ class MarcoChessVoice(MycroftSkill):
         async def update_emotion(self):
             while True:
                 self.outer_instance.client.request_emotion('Current')
-                self.outer_instance.last_emotion = self.outer_instance.ros2_call()
+                last_emotion = self.outer_instance.ros2_call()
+                if last_emotion not in 'NoFace':
+                    self.outer_instance.last_emotion = last_emotion
                 await asyncio.sleep(1)
 
     class AsyncTalker(object):
@@ -234,9 +267,13 @@ class MarcoChessVoice(MycroftSkill):
         async def talker(self):
             while True:
                 await asyncio.sleep(120)
-                converse = f'voice.emotion.marco.converse.{self.outer_instance.last_emotion}'
-                self.outer_instance.speak_dialog(converse,
-                                                 wait=True)
+                while True:
+                    if self.outer_instance.converse:
+                        converse = f'voice.emotion.marco.converse.{self.outer_instance.last_emotion}'
+                        self.outer_instance.speak_dialog(converse,
+                                                         data={'name': self.outer_instance.last_identity},
+                                                         wait=True)
+                        break
 
 
 def create_skill():
